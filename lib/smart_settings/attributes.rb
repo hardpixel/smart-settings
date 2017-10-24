655cab3c5e4bfa8eefe546732800bc316f623e15
Jonian Guveli <jonian@hardpixel.eu>
2017-10-24 11:01:52 +0300

    update attributes

--- lib/smart_settings/attributes.rb
+++ lib/smart_settings/attributes.rb
@@ -4,32 +4,32 @@ module SmartSettings
 
     included do
       class_attribute :setting_names
-      class_attribute :setting_group_names
+      class_attribute :setting_groups
 
-      self.setting_names       = []
-      self.setting_group_names = {}
+      self.setting_names  = []
+      self.setting_groups = {}
     end
 
     class_methods do
       def setting(name, type_cast=:string, options={})
         gname = options.delete(:group)
         sname = gname.nil? ? name : :"#{gname}_#{name}"
-        group = setting_group_names.fetch(gname, []) + [sname]
+        group = setting_groups.fetch(gname, []) + [sname]
 
         attribute sname, type_cast, options
 
-        self.setting_names      += sname
-        self.setting_group_names = setting_group_names.merge(gname => group)
+        self.setting_names += [sname]
+        self.setting_groups = setting_groups.merge(gname => group)
       end
     end
 
     def group_exists?(name)
-      setting_group_names.keys.include? name
+      setting_groups.keys.include? name
     end
 
     def group(name)
       if group_exists?(name)
-        keys = setting_group_names[name]
+        keys = setting_groups[name]
         data = attributes.symbolize_keys.select { |k, _v| k.in? keys }
 
         Hash[data.map { |k, v| [k.to_s.sub("#{name}_", '').to_sym, v] }]
